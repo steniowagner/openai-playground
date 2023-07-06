1e60cb31592bc43461d4e6a5da9728f06fd418e3
Stenio Wagner <stenio.wagner1@gmail.com>
2023-07-06 18:41:19 -0300

    feat: moving the class-based pinecone-handler to a functional-handler

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import path from "path";
 
 import { splitPDFInDocuments } from "./utils/split-pdf-in-documents";
 import { readPdf } from "./utils/read-pdf";
-import { PineconeHandler } from "./utils/pinecone-handler";
+import * as pinecone from "./utils/pinecone-handler";
 
 const PDF_FILE_PATH =
   "/Users/steniowagner/dev/openai-playground/test-files/test.pdf";
@@ -14,11 +14,10 @@ const run = async () => {
       throw new Error("Error when tried to read the pdf");
     }
     const pdfContentSplittedInDocuments = await splitPDFInDocuments(
-      path.basename(PDF_FILE_PATH),
-      pdfContent.text
+      pdfContent.text,
+      path.basename(PDF_FILE_PATH)
     );
-    const pineconHandler = new PineconeHandler();
-    await pineconHandler.storeDocuments(pdfContentSplittedInDocuments);
+    await pinecone.storeDocuments(pdfContentSplittedInDocuments);
   } catch (err) {
     console.log(err);
   }
