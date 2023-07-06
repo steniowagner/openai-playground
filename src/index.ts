5a8d4903f91964795c6bd3e0d324303f6800c885
Stenio Wagner <stenio.wagner1@gmail.com>
2023-07-06 15:04:42 -0300

    feat: splitting the input-pdf into documents using langchain

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,26 @@
+import path from "path";
+
 import { readPdf } from "./utils/read-pdf";
+import { splitPdfIntoDocuments } from "./utils/split-pdf-into-documents";
+
+const PDF_FILE_PATH =
+  "/Users/steniowagner/dev/openai-playground/test-files/test.pdf";
 
 const run = async () => {
-  const pdfContent = await readPdf(
-    "/Users/steniowagner/dev/openai-playground/test-files/test.pdf"
-  );
+  try {
+    const pdfName = path.basename(PDF_FILE_PATH);
+    const pdfContent = await readPdf(PDF_FILE_PATH);
+    if (!pdfContent) {
+      throw new Error("Error when tried to read the pdf");
+    }
+    const pdfContentSplittedIntoDocuments = await splitPdfIntoDocuments(
+      pdfContent.text,
+      pdfName
+    );
+    console.log(pdfContentSplittedIntoDocuments);
+  } catch (err) {}
 };
+
+(async () => {
+  await run();
+})();
---
+++ src/utils/split-pdf-into-documents.ts
@@ -0,0 +1,22 @@
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { Document } from "langchain/document";
+
+const CHUNK_OVERLAP = 200;
+const CHUNK_SIZE = 1000;
+
+export const splitPdfIntoDocuments = async (
+  pdfContent: string,
+  pdfFileName: string
+) => {
+  const textSplitter = new RecursiveCharacterTextSplitter({
+    chunkOverlap: CHUNK_OVERLAP,
+    chunkSize: CHUNK_SIZE,
+  });
+  const documents = await textSplitter.splitDocuments([
+    new Document({
+      metadata: { source: pdfFileName, type: "file" },
+      pageContent: pdfContent,
+    }),
+  ]);
+  return documents;
+};
